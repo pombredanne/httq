35e8e6474278229f92eba7c5e8dc9c6c71900d71
Nigel Small <nigel@nigelsmall.com>
2015-03-11 00:16:27 +0000

    _parse_header

--- httq.py
+++ httq.py
@@ -18,31 +18,10 @@ COLON = b":"
 
 
 if sys.version_info >= (3,):
-    DIGIT_0 = ord('0')
-    DIGIT_9 = ord('9')
+    SP_CHAR = ord(' ')
+
 else:
-    DIGIT_0 = b'0'
-    DIGIT_9 = b'9'
-
-
-def spliterate(s, sep=b" ", max_split=-1):
-    p = 0
-    try:
-        while max_split != 0:
-            q = s.index(sep, p)
-            yield s[p:q]
-            p = q + 1
-            if max_split > 0:
-                max_split -= 1
-    except ValueError:
-        pass
-    finally:
-        try:
-            eol = s.index(EOL, p)
-        except ValueError:
-            yield s[p:]
-        else:
-            yield s[p:eol]
+    SP_CHAR = b' '
 
 
 class HTTP(object):
@@ -55,9 +34,8 @@ class HTTP(object):
     # Response attributes
     status_code = None
     reason_phrase = None
-    headers = []
-    content_length = None
-    _content_type = None
+    _raw_headers = []
+    _parsed_headers = {}
 
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -85,44 +63,57 @@ class HTTP(object):
         f.writelines([GET, SP, url, SP, HTTP_1_1, EOL, self.host_header, EOL])
         f.flush()
 
+    def _parse_header(self, line, value_parser=None):
+        p = line.index(COLON)
+        key = line[:p].lower()
+        p += 1
+        while line[p] == SP_CHAR:
+            p += 1
+        q = line.index(CR, p)
+        try:
+            self._parsed_headers[key] = value_parser(line[p:q])
+        except (TypeError, ValueError):
+            self._parsed_headers[key] = line[p:q]
+
     def response(self):
         f = self.file
 
         # Status line
         status_line = f.readline()
-        p = status_line.index(b" ") + 1
-        q = status_line.index(b" ", p)
+        p = status_line.index(SP) + 1
+        q = status_line.index(SP, p)
         self.status_code = int(status_line[p:q])
         p = q + 1
         q = status_line.index(EOL, p)
         self.reason_phrase = status_line[p:q]
 
         # Headers
-        headers = self.headers
-        del headers[:]
-        self.content_length = None
-        self._content_type = None
+        self._parsed_headers.clear()
+        raw_headers = self._raw_headers
+        del raw_headers[:]
         while True:
             line = f.readline()
             if line == EOL:
                 break
             elif line.startswith(b"Content-Length:") or line.startswith(b"content-length:"):
-                content_length = line[15:].strip()
-                if DIGIT_0 <= content_length[0] <= DIGIT_9:
-                    self.content_length = int(content_length)
-                else:
-                    self.content_length = content_length
-            headers.append(line)
+                self._parse_header(line, int)
+            elif line.startswith(b"Transfer-Encoding:") or line.startswith(b"transfer-encoding:"):
+                self._parse_header(line)
+            raw_headers.append(line)
 
         return self.status_code
 
     @property
+    def content_length(self):
+        return self._parsed_headers.get(b"content-length")
+
+    @property
     def content_type(self):
-        if self._content_type is None:
-            for line in self.headers:
+        if b"content-type" not in self._parsed_headers:
+            for line in self._raw_headers:
                 if line.startswith(b"Content-Type:") or line.startswith(b"content-type:"):
-                    self._content_type = line[13:].strip()
-        return self._content_type
+                    self._parse_header(line)
+        return self._parsed_headers.get(b"content-type")
 
     def content(self):
         return self.file.read(self.content_length)
@@ -138,6 +129,8 @@ def test():
             print(http.reason_phrase)
             print(http.content_length)
             print(http.content_type)
+            print(http._raw_headers)
+            print(http._parsed_headers)
             content = http.content()
             print(content)
             print()
